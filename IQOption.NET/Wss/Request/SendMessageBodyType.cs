0219760b238c0a0a0e08dcfb87a22371e58e6e44
Jonathas Silva de Souza <jonathas.souza@xpi.com.br>
2021-01-03 16:29:17 -0300

    [FEAT] Change project to NET 5.0

---
+++ IQOption.NET/Wss/Request/SendMessageBodyType.cs
@@ -0,0 +1,10 @@
+namespace IqOptionApi.Ws.Request
+{
+    public struct RequestMessageBodyType
+    {
+        public const string GetCandles = "get-candles";
+
+        public const string OpenOptions = "binary-options.open-option";
+
+    }
+}
